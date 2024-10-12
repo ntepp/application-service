ccba7ac753676bb142480035d1b6be668fb3a4b0
ntepp <ntepp.marcus@gmail.com>
2024-10-12 17:57:33 +0200

    add micrometer for metrics

--- src/main/java/com/perinfinity/applicationservice/controller/CandidatureController.java
+++ src/main/java/com/perinfinity/applicationservice/controller/CandidatureController.java
@@ -2,6 +2,8 @@ package com.perinfinity.applicationservice.controller;
 
 import com.perinfinity.applicationservice.entity.Candidatures;
 import com.perinfinity.applicationservice.services.CandidatureService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,24 +12,26 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class CandidatureController {
     CandidatureService candidatureService;
-
-    public CandidatureController(CandidatureService candidatureService) {
+    private final Counter applicationCounter;
+    public CandidatureController(CandidatureService candidatureService, MeterRegistry registry) {
         this.candidatureService = candidatureService;
+        applicationCounter = Counter.builder("application.counter")
+                .description("Count the number of applications")
+                .tags("action", "post-application")
+                .register(registry);
     }
 
 
     @GetMapping("/{id}")
     public ResponseEntity<Candidatures> getCandidatures(@PathVariable Long id) {
         var candidature = candidatureService.getApplications(id);
-        if(candidature.isPresent()) {
-            return ResponseEntity.ok(candidature.get());
-        }
-        return ResponseEntity.notFound().build();
+        return candidature.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
     }
 
     @PostMapping
     public ResponseEntity<String> apply(@RequestBody Candidatures candidature) {
+        applicationCounter.increment();
         candidatureService.apply(candidature);
         return ResponseEntity.ok("SUCCESS");
     }
